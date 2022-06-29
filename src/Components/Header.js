a796863906d668720c23edf179223de7c59a9e06
Tyler Ashworth <tylerwashworth@gmail.com>
2022-06-28 20:10:43 -0600

    Added the Navigation.js file, brought in FontAwesome, and am still trying to figure out why flex won't work and isn't pushing the menu icon to the right side

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Navigation from "./Navigation";
 
 function Header() {
     return (
-        <header className="border-b font-bold p-3">
+        <header className="border-b p-3 flex justify-between items-center">
+           <span className="font-bold">
             AppNames
+            </span>
+
+            <Navigation/> 
         </header>
     )
 }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+function Navigation() {
+    const [showMenu, setShowMenu] = useState(false)
+
+    return (
+        <nav>
+            <span className="text-xl">
+                <FontAwesomeIcon 
+                    icon={faBars}
+                    onClick={() => setShowMenu(!showMenu)}
+                />
+            </span>
+        </nav>
+    )
+}
+
+
+export default Navigation
